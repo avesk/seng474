fbeed55af018a48a98b58d2e18675966d123b142
DerekSGov <Derekgsiemens@gmail.com>
2019-09-29 21:40:16 -0700

    Fixing conditional probability error

--- assignment1/naive_bayes.py
+++ assignment1/naive_bayes.py
@@ -9,31 +9,46 @@ Authors: Avery Kushner, Derek Siemens
 
 import math
 
-second_child_male = {
-	"2nd": 118.0/285.0,
-	"Child": 57.0/109,
-	"Male": 367.0/1731.0
+second_child_male_yes = {
+	"2nd": 118.0/771.0,
+	"Child": 57.0/771.0,
+	"Male": 367.0/771.0
+}
+second_child_male_no = {
+	"2nd": 167.0/1490.0,
+	"Child": 52.0/1490.0,
+	"Male": 1364.0/1490.0
 }
 
-second_adult_female = {
-	"2nd": 118.0/285.0,
-	"Adult": 654.0/2092.0,
-	"Female": 344.0/470.0
+second_adult_female_yes = {
+	"2nd": 118.0/771.0,
+	"Adult": 654.0/771.0,
+	"Female": 344.0/771.0
+}
+
+second_adult_female_no = {
+	"2nd": 167.0/1490.0,
+	"Adult": 1438.0/1490.0,
+	"Female": 126.0/1490.0
 }
 
 y_total_prob = 711.0/2201.0
 
-def naive_bayes(probabilities, pyes):
+def naive_bayes(cond_prob_yes, cond_prob_no, pyes):
 	y_probability_multiples = 1
 	n_probability_multiples = 1
 
-	for key in probabilities.keys():
-		y_probability_multiples*= probabilities[key]
-		n_probability_multiples*= (1 - probabilities[key])
+	for key in cond_prob_yes.keys():
+		y_probability_multiples *= cond_prob_yes[key]
+	for key in cond_prob_no.keys():
+		n_probability_multiples *= cond_prob_no[key]
 
 	y_probability_multiples*=pyes
 	n_probability_multiples*=(1-pyes)
+	print("P(survived | E ) alpha*{}".format(y_probability_multiples))
+	print("P(didnt survive | E ) alpha*{}".format(n_probability_multiples))
 	alpha = 1/(y_probability_multiples + n_probability_multiples)
+	print("Our Alpha Value is 1/P(survived| E) + P(didnt survive | E) ={}%".format(alpha))
 
 	return (y_probability_multiples*alpha*100, n_probability_multiples*alpha*100)
 
@@ -41,16 +56,18 @@ def naive_bayes(probabilities, pyes):
 def print_result():
 	print("#### Naive Bayes ####")
 	print("2nd child male ?")
-	pyes, pno = naive_bayes(second_child_male, y_total_prob)
+	pyes, pno = naive_bayes(second_child_male_yes, second_child_male_no, y_total_prob)
 	print("Probability of yes: {}%".format(pyes))
 	print("Probability of no: {}%".format(pno))
+	print('Thus classified as No')
 
 	print()
 
 	print("2nd adult female ?")
-	pyes, pno = naive_bayes(second_adult_female, y_total_prob)
+	pyes, pno = naive_bayes(second_adult_female_yes, second_child_male_no, y_total_prob)
 	print("Probability of yes: {}%".format(pyes))
 	print("Probability of no: {}%".format(pno))
+	print('Thus classified as Yes')
 
 print_result()
 
