ddfd3ef521c655281ccc11abe4f7ee264a116bd9
DerekSGov <Derekgsiemens@gmail.com>
2019-10-01 19:34:52 -0700

    Fixing data issues

--- assignment1/naive_bayes.py
+++ assignment1/naive_bayes.py
@@ -10,22 +10,20 @@ Authors: Avery Kushner, Derek Siemens
 import math
 
 second_child_male_yes = {
-	"2nd": 118.0/771.0,
-	"Child": 57.0/771.0,
-	"Male": 367.0/771.0
+	"2nd": 118.0/711.0,
+	"Child": 57.0/711.0,
+	"Male": 367.0/711.0
 }
 second_child_male_no = {
 	"2nd": 167.0/1490.0,
 	"Child": 52.0/1490.0,
 	"Male": 1364.0/1490.0
 }
-
 second_adult_female_yes = {
-	"2nd": 118.0/771.0,
-	"Adult": 654.0/771.0,
-	"Female": 344.0/771.0
+	"2nd": 118.0/711.0,
+	"Adult": 654.0/711.0,
+	"Female": 344.0/711.0
 }
-
 second_adult_female_no = {
 	"2nd": 167.0/1490.0,
 	"Adult": 1438.0/1490.0,
@@ -48,7 +46,7 @@ def naive_bayes(cond_prob_yes, cond_prob_no, pyes):
 	print("P(survived | E ) alpha*{}".format(y_probability_multiples))
 	print("P(didnt survive | E ) alpha*{}".format(n_probability_multiples))
 	alpha = 1/(y_probability_multiples + n_probability_multiples)
-	print("Our Alpha Value is 1/P(survived| E) + P(didnt survive | E) ={}%".format(alpha))
+	print("Our Alpha Value is 1/P(survived| E) + P(didnt survive | E) ={}".format(alpha))
 
 	return (y_probability_multiples*alpha*100, n_probability_multiples*alpha*100)
 
@@ -64,7 +62,7 @@ def print_result():
 	print()
 
 	print("2nd adult female ?")
-	pyes, pno = naive_bayes(second_adult_female_yes, second_child_male_no, y_total_prob)
+	pyes, pno = naive_bayes(second_adult_female_yes, second_adult_female_no, y_total_prob)
 	print("Probability of yes: {}%".format(pyes))
 	print("Probability of no: {}%".format(pno))
 	print('Thus classified as Yes')
