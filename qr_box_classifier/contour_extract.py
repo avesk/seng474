b55e53b58ecd259e39beb5fc6abbf11fe2255642
avesk <averykushner@gmail.com>
2019-11-17 14:02:37 -0800

    extracting all contours greater than 2 vertices

--- qr_box_classifier/contour_extract.py
+++ qr_box_classifier/contour_extract.py
@@ -26,32 +26,45 @@ def detect_shape(contour):
         x, y, w, h = cv2.boundingRect(approx)
         ar = w / float(h)
         if ar >= 1.0-ar_tol and ar <= 1.0+ar_tol and h > 5:
-            print('found square')
             return (x, y)
     return -1, -1
+
+
+def filter_contour(contour):
+    curve_len = cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, 0.05 * curve_len, True)
+    if len(approx) >= 2 and len(approx) < 50:
+        x, y, w, h = cv2.boundingRect(approx)
+        if h > 5 and h < 1900:
+            return (x, y)
+
+    return (-1, -1)
 
 
 win_name = 'frame'
 create_window(win_name)
 
-filepath = 'training-data/4.jpg'
+filepath = 'training-data/25.jpg'
+# filepath = 'training-data/4.jpg'
+# filepath = 'training-data/47.jpg'
 
 
 frame = cv2.imread(filepath)
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-thresh = cv2.adaptiveThreshold(
-    src=gray, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    thresholdType=cv2.THRESH_BINARY, blockSize=3, C=5
-)
+# thresh = cv2.adaptiveThreshold(
+#     src=gray, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+#     thresholdType=cv2.THRESH_BINARY, blockSize=5, C=5
+# )
+ret, thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
 
 contours, h = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 for cont in contours:
-    x, y = detect_shape(cont)
-    # if x > -1:
-    #     print(cont)
-    #     cv2.drawContours(frame, [cont], 0, (0, 255, 0), 2)
-    #     print(x, y)
-    cv2.drawContours(frame, [cont], 0, (0, 255, 0), 2)
+    x, y = filter_contour(cont)
+    if x > -1:
+        print(cont)
+        cv2.drawContours(frame, [cont], 0, (0, 255, 0), 2)
+        print(x, y)
+    # cv2.drawContours(frame, [cont], 0, (0, 255, 0), 2)
 # cv2.imshow(win_name, thresh)
 cv2.imshow(win_name, frame)
 cv2.waitKey(0)
