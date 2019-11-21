ce732bcba59d82c4aa035c8b00950c13b905afe7
avesk <averykushner@gmail.com>
2019-11-20 16:03:25 -0800

    filters training image segments on .jpg extension

--- qr_box_classifier/train.py
+++ qr_box_classifier/train.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
-import imutils
 import os
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix
 import random
 from sklearn import svm
+from sklearn import tree
 
 
 def filter_contour(contour):
@@ -14,7 +14,7 @@ def filter_contour(contour):
     approx = cv2.approxPolyDP(contour, 0.05 * curve_len, True)
     if len(approx) >= 2 and len(approx) < 50:
         x, y, w, h = cv2.boundingRect(approx)
-        if h > 5 and h < 1900:
+        if h > 20 and h < 1900:
             return (x, y)
 
     return (-1, -1)
@@ -23,27 +23,28 @@ def filter_contour(contour):
 def feature_extract(cls, directory, documents):
     row_len = 216
     for filename in os.listdir(directory):
-        doc = [0.0, 0.0, 0.0]
-        frame = cv2.imread(f"{directory}/{filename}")
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
+        if filename.endswith(".jpg"):
+            doc = [0.0, 0.0, 0.0]
+            frame = cv2.imread(f"{directory}/{filename}")
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            ret, thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
 
-        contours, h = cv2.findContours(
-            thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours, h = cv2.findContours(
+                thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        areas = []
-        for i, cont in enumerate(contours):
-            x, y = filter_contour(cont)
-            if x > -1:
-                areas.append(cv2.contourArea(cont))
-        if len(areas) > 0:
-            mean = np.mean(areas)
-            std = np.std(areas)
-            card = len(areas)
-            doc[0] = mean
-            doc[1] = std
-            doc[2] = float(card)
-        documents[cls].append(doc)
+            areas = []
+            for i, cont in enumerate(contours):
+                x, y = filter_contour(cont)
+                if x > -1:
+                    areas.append(cv2.contourArea(cont))
+            if len(areas) > 0:
+                mean = np.mean(areas)
+                std = np.std(areas)
+                card = len(areas)
+                doc[0] = mean
+                doc[1] = std
+                doc[2] = float(card)
+            documents[cls].append(doc)
     return documents
 
 
@@ -106,5 +107,13 @@ print(conf_matrix)
 score, conf_matrix = train(X, Y, svm.SVC(gamma='scale'))
 
 print("SVM")
+print(score)
+print(conf_matrix)
+
+
+# Decision Tree
+score, conf_matrix = train(X, Y, tree.DecisionTreeClassifier())
+
+print("Tree")
 print(score)
 print(conf_matrix)
